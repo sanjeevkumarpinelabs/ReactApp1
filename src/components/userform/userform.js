b3e9ea616461ee7c59dd8370e2d623c77e7b11e3
Sanjeev Kumar <sanjeev.kumar@pinelabs.com>
2021-01-09 09:51:34 +0530

    populated users array and used map to iterate and populate list

--- src/components/userform/userform.js
+++ src/components/userform/userform.js
@@ -11,8 +11,11 @@ export class UserForm extends React.Component{
         super(props);
         console.log(this.props);
         this.state = {
+            user :{
             fname:"Sanjeev",
             lname:'Kumar'
+            },
+            users: [{fname:"Ramesh" , lname:"Kumar"}, {fname: "Dinesh" , lname: "Sinha"}]
         }
     }
 
@@ -21,68 +24,37 @@ export class UserForm extends React.Component{
     save =  event => {
         //console.log(event);
       //  console.log(this.props);
-        console.log(this.state.fname);
+        console.log(this.state.user.fname);
         console.log(this);
 
-        this.setState({
-            lname:this.state.fname 
-        });
+        // this.setState({
+        //     lname:this.state.fname 
+        // });
     }
 
     handleEvent = (event) => {
         //const propertyName = event.target.name;
         this.setState({
             //property name can not have dots hence angled bracket is a must
-            [event.target.name]: event.target.value
+            user: {[event.target.name]: event.target.value}
             //propertyName :event.target.value 
         });
         console.log(`name = ${event.target.name} and the value = ${event.target.value}`);
     }
     render(){
-        // return (
-        //     <div>
-                
-        //         <label>
-        //             First Name:    
-        //         <input value = {this.state.fname} onChange={(event) => {
-        //             console.log(this.state.fname);
-        //            //this.state.fname = event.target.value;
-        //             this.setState({
-        //                 fname:event.target.value
-        //             });
-        //             console.log(event);
-        //         }}
-        //         placeholder={this.props.label} style={{backgroundColor: this.props.color}}/>
-            
-        //         </label>
-                
-        //         <input value = {this.state.lname} onChange={(event)=> {
-        //             this.setState({
-        //                 lname:event.target.value
-        //             });
-        //             console.log(`last name is ${this.state.lname}`);
-        //             // console.log(`current state value is ${this.state}`);
-        //             }} placeholder='first name copy' style={{backgroundColor: this.props.color}}/>
-        //         {/* <button onClick = {(event)=>{
-        //             console.log(event);
-        //         }}>Save </button> */}
-        //         <button onClick ={this.save}>SaveNew </button>
-        //     </div>
-        //     )
-
         return (
             <div>
                 
                 <label>
                     First Name:    
-                <input value = {this.state.fname} name= "fname" onChange = {this.handleEvent}
+                <input value = {this.state.user.fname} name= "fname" onChange = {this.handleEvent}
                 placeholder={this.props.label} style={{backgroundColor: this.props.color}}/>
             
                 </label>
                 <br/> <br/>
                 <label>
                     Last Name:
-                    <input value = {this.state.lname} name= "lname" onChange = {this.handleEvent} 
+                    <input value = {this.state.user.lname} name= "lname" onChange = {this.handleEvent} 
                     placeholder='first name copy' style={{backgroundColor: this.props.color}}/>
                 </label>
                 <br/>
@@ -96,14 +68,12 @@ export class UserForm extends React.Component{
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                    <td>Sanjeev</td>
-                                    <td>Kumar</td>
-                            </tr>
-                            <tr>
-                                    <td>Sneh</td>
-                                    <td>Mishra</td>
-                            </tr>
+                           {this.state.users.map(user => {
+                              // console.log(user);
+                              return <tr> <td> {user.fname} </td> <td> {user.lname} </td> </tr>
+
+                               // return <tr>test</tr>;
+                           })}
 
                         </tbody>
                 </table>
