5d21638216d296d3ea717b05ac1b6828c7ac7f4d
Sanjeev Kumar <sanjeev.kumar@pinelabs.com>
2021-01-18 08:49:18 +0530

    Routing demo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 //import logo from './logo.svg';
+import {Route, Switch ,BrowserRouter as Router,Link} from 'react-router-dom'
 import './App.css';
 import { UserForm } from './components/userform/userform'
-
+import Home from './components/home'
 function App() {
   return (
     <div className="App">
-     <UserForm label='first name' color='cyan'/>
-     {/* <UserForm label = 'last name' color='yellow'/>
-     <UserForm label = 'age' color = 'blue'/> */}
+      <Router>
+        <nav>
+          <ul>
+              <li> 
+                <Link to="home">Home</Link> 
+              </li>
+              <li> 
+                <Link to="UserForm">UserForm</Link> 
+              </li>
+          </ul>
+        </nav>
+        <Switch>
+            <Route exact path='/home'>
+              <Home></Home>
+            </Route>
+            <Route exact path='/userform'>
+               <UserForm label='first name' color='green'/>
+            </Route>
+        </Switch>
+      </Router>
+     {/* <UserForm label='first name' color='cyan'/> */}
     </div>
   );
 }
